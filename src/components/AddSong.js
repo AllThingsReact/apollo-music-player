7f03586b8fb5487ca39f8e9855d8dadd0457f722
meetpriyanshu@gmail.com <hervingLeCOtionIo>
2020-12-12 22:49:01 +0530

    Add song Modal dialog

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,7 +1,98 @@
-import React from "react";
+import {
+  InputAdornment,
+  TextField,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@material-ui/core";
+import { AddBoxOutlined, Link } from "@material-ui/icons";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    alignItems: "center",
+  },
+  urlInput: {
+    margin: theme.spacing(1),
+  },
+  addSongButton: {
+    margin: theme.spacing(1),
+  },
+  dialog: {
+    textAlign: "center",
+  },
+  thumbnail: {
+    width: "90%",
+  },
+}));
 
 function AddSong() {
-  return <div>addSong</div>;
+  const classes = useStyles();
+  const [dialog, setDialog] = useState(false);
+
+  const handleCloseDialog = () => {
+    setDialog(false);
+  };
+
+  return (
+    <div className={classes.container}>
+      <Dialog
+        open={dialog}
+        onClose={handleCloseDialog}
+        className={classes.dialog}
+      >
+        <DialogTitle>Edit Song</DialogTitle>
+        <DialogContent>
+          <img alt="Song Thumbnail" className={classes.thumbnail} />
+          <TextField margin="dense" name="title" label="Title" fullWidth />
+          <TextField margin="dense" name="artist" label="Artist" fullWidth />
+          <TextField
+            margin="dense"
+            name="thumbnail"
+            label="Thumbnail"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="secondary">
+            Cancel
+          </Button>
+          <Button variant="outlined" color="primary">
+            Add Song
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <TextField
+        className={classes.urlInput}
+        placeholder="Add url"
+        fullWidth
+        margin="normal"
+        type="url"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Link />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Button
+        className={classes.addSongButton}
+        onClick={() => {
+          setDialog(true);
+        }}
+        variant="contained"
+        color="primary"
+        endIcon={<AddBoxOutlined />}
+      >
+        Add
+      </Button>
+    </div>
+  );
 }
 
 export default AddSong;
